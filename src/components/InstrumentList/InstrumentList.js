644222d15d5e8980170eab48c7660eeca8d93d96
itsyme <e0524723@u.nus.edu>
2022-01-04 23:14:11 +0800

    Update statistics

--- src/components/InstrumentList/InstrumentList.js
+++ src/components/InstrumentList/InstrumentList.js
@@ -4,13 +4,22 @@ import './InstrumentList.css';
 
 function InstrumentList() {
     return (
-        <div className='instrument-list__background'>
-            <InstrumentTile className='instrument-list__instrument-tile'/>
-            <InstrumentTile className='instrument-list__instrument-tile'/>
-            <InstrumentTile className='instrument-list__instrument-tile'/>
-            <InstrumentTile className='instrument-list__instrument-tile'/>
-            <InstrumentTile className='instrument-list__instrument-tile'/>
-            <InstrumentTile className='instrument-list__instrument-tile'/>
+        <div className='instrument-list__container'>
+            <div className='instrument-list__background'>
+                <InstrumentTile className='instrument-list__instrument-tile'/>
+                <InstrumentTile className='instrument-list__instrument-tile'/>
+                <InstrumentTile className='instrument-list__instrument-tile'/>
+                <InstrumentTile className='instrument-list__instrument-tile'/>
+                <InstrumentTile className='instrument-list__instrument-tile'/>
+                <InstrumentTile className='instrument-list__instrument-tile'/>
+            </div>
+            <div className='instrument-list__statistics-background'>
+                <div className='instrument-list__statistics'>
+                    <h1>Profit:</h1>
+                    <h2>Total Value:</h2>
+                    <h2>Total Input:</h2>
+                </div>
+            </div>
         </div>
     );
 }
